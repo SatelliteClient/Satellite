8c2f81f9007d5e363dfce60368af0a273a334fb7
nardpw <82015149+nardpw@users.noreply.github.com>
2021-04-06 16:47:33 +0900

    Create Client.java

---
+++ src/main/satellite/Client.java
@@ -0,0 +1,15 @@
+package satellite;
+
+public class Client {
+	
+	public static String name = "Satellite", version = "1";
+	
+	public static void init() {
+		
+	}
+	
+	public static void keyPress(int key) {
+		
+	}
+	
+}
