0104717b664101f70fea2828e9e6059615a444a3
nardpw <82015149+nardpw@users.noreply.github.com>
2021-04-07 11:50:31 +0900

    Update Fly.java

--- src/main/satellite/module/movement/Fly.java
+++ src/main/satellite/module/movement/Fly.java
@@ -2,6 +2,8 @@ package satellite.module.movement;
 
 import org.lwjgl.input.Keyboard;
 
+import satellite.event.Event;
+import satellite.event.listeners.EventUpdate;
 import satellite.module.Module;
 
 public class Fly extends Module {
@@ -9,7 +11,21 @@ public class Fly extends Module {
 	public Fly() {
 		super("Fly", Keyboard.KEY_F, Category.MOVEMENT);
 	}
-
 	
+	@Override
+	public void onDisable() {
+		mc.player.capabilities.isFlying = false;
+		super.onDisable();
+	}
+
+	@Override
+	public void onEvent(Event e) {
+		if(e instanceof EventUpdate) {
+			if(e.isPre()) {
+				mc.player.capabilities.isFlying = true;
+			}
+		}
+		super.onEvent(e);
+	}
 	
 }
