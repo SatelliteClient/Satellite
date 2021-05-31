03d8e1a7c15f3a4c0475b184223fb784c1ae7fc8
nardpw <82015149+nardpw@users.noreply.github.com>
2021-05-31 13:56:04 +0900

    Create Satellite.java

---
+++ src/main/java/com/github/satellite/Satellite.java
@@ -0,0 +1,84 @@
+package com.github.satellite;
+
+import com.github.satellite.network.SatelliteNetClient;
+import com.github.satellite.event.Event;
+import com.github.satellite.features.module.Module;
+import com.github.satellite.features.module.ModuleManager;
+import com.github.satellite.ui.HUD;
+import com.github.satellite.ui.clickGUI.GuiClickGUI;
+import com.github.satellite.ui.theme.ThemeManager;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.ClientChatEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import org.lwjgl.input.Keyboard;
+
+@Mod(modid = Satellite.MOD_ID, name = Satellite.NAME, version = Satellite.VERSION)
+public class Satellite
+{
+    public static final String MOD_ID = "satellite";
+    public static final String NAME = "Satellite Utility";
+    public static final String VERSION = "1.1.1";
+
+	public static HUD hud = new HUD();
+	public static SatelliteNetClient SatelliteNet;
+	public static boolean isConnected;
+	public static ThemeManager themeManager = new ThemeManager();
+	public static Minecraft mc = Minecraft.getMinecraft();
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		MinecraftForge.EVENT_BUS.register(this);
+
+		try {
+			SatelliteNet = new SatelliteNetClient();
+			SatelliteNet.sendLoginPacket();
+			isConnected=true;
+		}catch(Exception e) {
+			isConnected=false;
+		}
+
+		ModuleManager.registerModules();
+		GuiClickGUI.loadModules();
+	}
+
+	public static void onEvent(Event<?> e) {
+		ModuleManager.modules.stream().forEach(m -> {
+			if(m.isEnable()) m.onEvent(e);
+		});
+	}
+
+	@SubscribeEvent
+	public void keyEvent(InputEvent.KeyInputEvent e) {
+		if (mc.currentScreen == null) {
+			try {
+				if (Keyboard.isCreated()) {
+					if (Keyboard.getEventKeyState()) {
+						int i = Keyboard.getEventKey();
+						if (i != 0) {
+							ModuleManager.modules.stream().forEach(m -> {
+								if(m.getKeyCode() == i) m.toggle();
+							});
+						}
+					}
+				}
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void chatEvent(ClientChatEvent event) {
+		String message = event.getMessage();
+
+		if (message.startsWith(".")) {
+			event.setCanceled(true);
+		}
+	}
+}
