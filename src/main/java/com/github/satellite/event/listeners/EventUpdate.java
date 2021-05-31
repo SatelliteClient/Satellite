dde15da8a2494fa7025f718c58376ce75857a183
nardpw <82015149+nardpw@users.noreply.github.com>
2021-05-31 14:06:06 +0900

    Delete src/main directory

--- src/main/java/com/github/satellite/event/listeners/EventUpdate.java
+++
@@ -1,9 +0,0 @@
-package com.github.satellite.event.listeners;
-
-import com.github.satellite.event.Event;
-
-public class EventUpdate extends Event<EventUpdate> {
-
-	
-	
-}
--- src/main/java/com/github/satellite/network/packet/packets/CPLogin.java
+++
@@ -1,4 +0,0 @@
-package com.github.satellite.network.packet.packets;
-
-public class CPLogin {
-}
