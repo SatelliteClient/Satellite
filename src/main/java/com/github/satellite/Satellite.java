00e017415cf779c6dfa24f2919f2b0d01f09b48c
nardpw <82015149+nardpw@users.noreply.github.com>
2021-06-23 07:07:36 +0900

    2.0.3

--- src/main/java/com/github/satellite/Satellite.java
+++ src/main/java/com/github/satellite/Satellite.java
@@ -28,7 +28,7 @@ public class Satellite
 {
     public static final String MOD_ID = "satellite";
     public static final String NAME = "Satellite Utility";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "2.0.3";
 
 	public static HUD hud = new HUD();
 	public static ThemeManager themeManager = new ThemeManager();
