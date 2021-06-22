df5b522f9543045b67820736f101eebcf8a84b5b
nardpw <82015149+nardpw@users.noreply.github.com>
2021-06-22 20:05:20 +0900

    2.0.2
    -FixHoleKIcker

--- src/main/java/com/github/satellite/features/module/combat/HoleKicker.java
+++ src/main/java/com/github/satellite/features/module/combat/HoleKicker.java
@@ -1,74 +1,123 @@
 package com.github.satellite.features.module.combat;
 
 import com.github.satellite.event.Event;
+import com.github.satellite.event.listeners.EventMotion;
 import com.github.satellite.event.listeners.EventUpdate;
 import com.github.satellite.features.module.Module;
+import com.github.satellite.utils.BlockUtils;
+import com.github.satellite.utils.InventoryUtils;
+
 import net.minecraft.block.BlockAir;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.InventoryBasic;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import org.lwjgl.input.Keyboard;
 
 public class HoleKicker extends Module {
-	
+
 	public HoleKicker() {
 		super("HoleKicker", Keyboard.KEY_NONE, Category.COMBAT);
 	}
-	
-    @Override
-    public void init() {
-    	super.init();
-    }
+
+	@Override
+	public void init() {
+		super.init();
+	}
+
+	int progress = 0;
+
+	EnumFacing facing;
+
+	@Override
+	public void onEnable() {
+		progress = 0;
+		super.onEnable();
+	}
 
 	@Override
 	public void onEvent(Event<?> e) {
-		if(e instanceof EventUpdate) {
+		if (e instanceof EventUpdate) {
 			for(Entity entity : mc.world.loadedEntityList) {
 				if(entity == mc.player) continue;
-				
-				if(entity instanceof EntityLivingBase) {
+
+				if(entity instanceof EntityPlayer) {
+					int piston = InventoryUtils.pickItem(33);
+					int power = InventoryUtils.pickItem(152);
+
+					BlockPos pos = new BlockPos(entity).offset(EnumFacing.UP);
+
+					if (piston == -1 || power == -1 || progress < 2) {
+						facing = getFacing(pos);
+						if (facing != null) {
+							progress++;
+						}else {
+							progress = 0;
+						}
+						return;
+					}
+					BlockUtils event = BlockUtils.isPlaceable(pos.offset(facing), 0, true);
+					if (event != null) {
+						mc.player.inventory.currentItem = piston;
+						if (!event.doPlace(true)) {
+							return;
+						}
+
+
+						mc.player.inventory.currentItem = power;
+						for (EnumFacing f : EnumFacing.values()) {
+							pos = new BlockPos(entity).offset(EnumFacing.UP).offset(facing);
+							event = BlockUtils.isPlaceable(pos.offset(f), 0, true);
+							if (BlockUtils.doPlace(event, true)) {
+								//progress = 0;
+								return;
+							}
+						}
+					}
 				}
+			}
+		}
+		if (e instanceof EventMotion) {
+			EventMotion event = (EventMotion)e;
+
+			if (progress > 0) {
+				switch (facing) {
+
+					case NORTH:
+						event.yaw = 180;
+						break;
+					case SOUTH:
+						event.yaw = 0;
+						break;
+					case WEST:
+						event.yaw = 90;
+						break;
+					case EAST:
+						event.yaw = -90;
+						break;
+
+				}
+				event.pitch = 0;
+				progress++;
 			}
 		}
 		super.onEvent(e);
 	}
-	
-	/**
-	 * @param entity
-	 * @return
-	 */
-	public boolean canKickable(Entity entity) {
-		BlockPos head = new BlockPos(new Vec3d(entity.posX, entity.posY+entity.height, entity.posZ));
-		if(!(mc.world.getBlockState(head).getBlock() instanceof BlockAir)) {
-			return false;
+
+	public EnumFacing getFacing(BlockPos position) {
+		for (EnumFacing f : EnumFacing.values()) {
+			BlockPos pos = new BlockPos(position).offset(f);
+
+			if (pos.getY() != position.getY())
+				continue;
+
+			if (mc.world.isAirBlock(pos)) {
+				return f;
+			}
 		}
-		
-		if(mc.world.getBlockState(slide(head, EnumFacing.NORTH)).getBlock() instanceof BlockAir) {
-			head.offset(EnumFacing.NORTH);
-			return true;
-		}
-		else if(mc.world.getBlockState(slide(head, EnumFacing.SOUTH)).getBlock() instanceof BlockAir) {
-			head.offset(EnumFacing.SOUTH);
-			return true;
-		}
-		else if(mc.world.getBlockState(slide(head, EnumFacing.WEST)).getBlock() instanceof BlockAir) {
-			head.offset(EnumFacing.WEST);
-			return true;
-		}
-		else if(mc.world.getBlockState(slide(head, EnumFacing.EAST)).getBlock() instanceof BlockAir) {
-			head=head.offset(EnumFacing.EAST);
-			return true;
-		}
-		else return false;
-	}
-	
-	private BlockPos slide(BlockPos pos, EnumFacing f) {
-		return new BlockPos(
-				pos.getX()+f.getDirectionVec().getX(),
-				pos.getY()+f.getDirectionVec().getY(),
-				pos.getZ()+f.getDirectionVec().getZ()
-				);
+		return null;
 	}
 }
--- src/main/java/com/github/satellite/utils/BlockUtils.java
+++ src/main/java/com/github/satellite/utils/BlockUtils.java
@@ -2,6 +2,7 @@ package com.github.satellite.utils;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockAir;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemBlock;
@@ -14,15 +15,22 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 
 public class BlockUtils {
-	
+
 	protected static Minecraft mc = Minecraft.getMinecraft();
-	
+
 	public BlockPos pos;
 	public int a;
 	public EnumFacing f;
 	public double dist;
 	public double rotx, roty;
-	
+
+	public static boolean doPlace(BlockUtils event, boolean swing) {
+		if (event == null)
+			return false;
+
+		return event.doPlace(swing);
+	}
+
 	public static BlockUtils isPlaceable(BlockPos pos, double dist, boolean Collide) {
 		BlockUtils event = new BlockUtils(pos, 0, null, dist);
 
@@ -33,13 +41,29 @@ public class BlockUtils {
 		//    return EnumActionResult.FAIL;
 		if(!(mc.player.inventory.getCurrentItem().getItem() instanceof ItemBlock)) return null;
 
+		AxisAlignedBB axisalignedbb = ((ItemBlock)mc.player.inventory.getCurrentItem().getItem()).getBlock().getDefaultState().getCollisionBoundingBox(mc.world, pos);
+
+		if(!isAir(pos)) {
+			if (mc.world.getBlockState(pos).getBlock() instanceof BlockLiquid) {
+				Block block = mc.world.getBlockState(pos.offset(EnumFacing.UP)).getBlock();
+				if (block instanceof BlockLiquid) {
+					event.f = EnumFacing.DOWN;
+					event.pos.offset(EnumFacing.UP);
+					return event;
+				}else {
+					event.f = EnumFacing.UP;
+					event.pos.offset(EnumFacing.DOWN);
+					return event;
+				}
+			}
+		}
+
 		for(EnumFacing f : EnumFacing.values())
 		{
 			if(!isAir(new BlockPos(pos.getX()-f.getDirectionVec().getX(), pos.getY()-f.getDirectionVec().getY(), pos.getZ()-f.getDirectionVec().getZ())))
 			{
 				event.f=f;
 
-				AxisAlignedBB axisalignedbb = ((ItemBlock)mc.player.inventory.getCurrentItem().getItem()).getBlock().getDefaultState().getCollisionBoundingBox(mc.world, pos);
 
 				if (Collide && axisalignedbb != Block.NULL_AABB && !mc.world.checkNoEntityCollision(axisalignedbb.offset(pos), (Entity)null))
 				{
@@ -55,8 +79,6 @@ public class BlockUtils {
 			event.pos.offset(EnumFacing.UP);
 			pos.offset(EnumFacing.DOWN);
 
-			AxisAlignedBB axisalignedbb = ((ItemBlock)mc.player.inventory.getCurrentItem().getItem()).getBlock().getDefaultState().getCollisionBoundingBox(mc.world, pos);
-
 			if (Collide && axisalignedbb != Block.NULL_AABB && !mc.world.checkNoEntityCollision(axisalignedbb.offset(pos), (Entity)null))
 			{
 				return null;
@@ -70,14 +92,14 @@ public class BlockUtils {
 	public static boolean isAir(BlockPos pos)
 	{
 		Block block = mc.world.getBlockState(pos).getBlock();
-		return block.isReplaceable(mc.world, pos) && block instanceof BlockAir;
+		return block instanceof BlockAir;
 	}
 	public static boolean isRePlaceable(BlockPos pos)
 	{
 		Block block = mc.world.getBlockState(pos).getBlock();
 		return block.isReplaceable(mc.world, pos) && !(block instanceof BlockAir);
 	}
-	
+
 	public BlockUtils(BlockPos pos, int a, EnumFacing f, double dist) {
 		this.pos=pos;
 		this.a=a;
@@ -85,49 +107,53 @@ public class BlockUtils {
 		this.dist=dist;
 	}
 
-	public void doPlace(boolean swing) {
+	public boolean doPlace(boolean swing) {
 		double dx=((pos.getX()+0.5-mc.player.posX) - ((double)f.getDirectionVec().getX())/2 );
 		double dy=((pos.getY()+0.5-mc.player.posY) - ((double)f.getDirectionVec().getY())/2 )-mc.player.getEyeHeight();
 		double dz=((pos.getZ()+0.5-mc.player.posZ) - ((double)f.getDirectionVec().getZ())/2 );
 
 		double x=getDirection2D(dz, dx);
 		double y=getDirection2D(dy, Math.sqrt(dx*dx+dz*dz));
-		
+
 		Vec3d vec = getVectorForRotation(-y, x-90);
-		
+
 		this.roty=-y;
 		this.rotx=x-90;
-		
+
 		EnumActionResult enumactionresult = mc.playerController.processRightClickBlock(mc.player, mc.world, new BlockPos(pos.getX() - f.getDirectionVec().getX(), pos.getY() - f.getDirectionVec().getY(), pos.getZ() - f.getDirectionVec().getZ()), f, vec, EnumHand.MAIN_HAND);
 
-        if (enumactionresult == EnumActionResult.SUCCESS && swing)
-        {
-            mc.player.swingArm(EnumHand.MAIN_HAND);
-        }
+		if (enumactionresult == EnumActionResult.SUCCESS)
+		{
+			if (swing)
+				mc.player.swingArm(EnumHand.MAIN_HAND);
+
+			return true;
+		}
+		return false;
 	}
-	
+
 	public void doBreak() {
 		mc.playerController.onPlayerDamageBlock(new BlockPos(pos.getX() - f.getDirectionVec().getX(), pos.getY() - f.getDirectionVec().getY(), pos.getZ() - f.getDirectionVec().getZ()), f);
 	}
-	
-    protected final Vec3d getVectorForRotation(float pitch, float yaw)
-    {
-        float f = MathHelper.cos(-yaw * 0.017453292F - (float)Math.PI);
-        float f1 = MathHelper.sin(-yaw * 0.017453292F - (float)Math.PI);
-        float f2 = -MathHelper.cos(-pitch * 0.017453292F);
-        float f3 = MathHelper.sin(-pitch * 0.017453292F);
-        return new Vec3d((double)(f1 * f2), (double)f3, (double)(f * f2));
-    }
-    
-    protected final Vec3d getVectorForRotation(double pitch, double yaw)
-    {
-        float f = MathHelper.cos((float) (-yaw * 0.017453292F - (float)Math.PI));
-        float f1 = MathHelper.sin((float) (-yaw * 0.017453292F - (float)Math.PI));
-        float f2 = -MathHelper.cos((float) (-pitch * 0.017453292F));
-        float f3 = MathHelper.sin((float) (-pitch * 0.017453292F));
-        return new Vec3d((double)(f1 * f2), (double)f3, (double)(f * f2));
-    }
-    
+
+	protected final Vec3d getVectorForRotation(float pitch, float yaw)
+	{
+		float f = MathHelper.cos(-yaw * 0.017453292F - (float)Math.PI);
+		float f1 = MathHelper.sin(-yaw * 0.017453292F - (float)Math.PI);
+		float f2 = -MathHelper.cos(-pitch * 0.017453292F);
+		float f3 = MathHelper.sin(-pitch * 0.017453292F);
+		return new Vec3d((double)(f1 * f2), (double)f3, (double)(f * f2));
+	}
+
+	protected final Vec3d getVectorForRotation(double pitch, double yaw)
+	{
+		float f = MathHelper.cos((float) (-yaw * 0.017453292F - (float)Math.PI));
+		float f1 = MathHelper.sin((float) (-yaw * 0.017453292F - (float)Math.PI));
+		float f2 = -MathHelper.cos((float) (-pitch * 0.017453292F));
+		float f3 = MathHelper.sin((float) (-pitch * 0.017453292F));
+		return new Vec3d((double)(f1 * f2), (double)f3, (double)(f * f2));
+	}
+
 	public static double getDirection2D(double dx, double dy)
 	{
 		double d;
