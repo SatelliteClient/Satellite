bd9e3901a2f50bcfdacf42a0d3c33726672b9cf1
Yajii2 <yu.sute22@gmail.com>
2021-06-22 13:51:35 +0900

    Update Yaw.java

--- src/main/java/com/github/satellite/command/impl/Yaw.java
+++ src/main/java/com/github/satellite/command/impl/Yaw.java
@@ -1,7 +1,6 @@
 package com.github.satellite.command.impl;
 
 import com.github.satellite.command.Command;
-import net.minecraft.client.Minecraft;
 import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.TextComponentString;
 
@@ -12,16 +11,18 @@ public class Yaw extends Command {
     }
 
     @Override
-    public boolean onCommand(String[] argsa, String command) {
+    public boolean onCommand(String[] args, String command) {
         if (args.length < 1) {
-            mc.ingameGUI.addChatMessage(ChatType.SYSTEM, new TextComponentString("yaw: " + mc.player.cameraYaw));
+            mc.ingameGUI.addChatMessage(ChatType.SYSTEM, new TextComponentString("yaw: " + mc.player.rotationYaw));
+            return true;
         } else {
             try {
-                mc.player.cameraYaw = Float.parseFloat(args[0]);
+                mc.player.rotationYaw = Float.parseFloat(args[0]);
+                return true;
             } catch (NumberFormatException e) {
                 mc.ingameGUI.addChatMessage(ChatType.SYSTEM, new TextComponentString(args[0] + " is not number"));
             }
         }
+        return false;
     }
-
 }
